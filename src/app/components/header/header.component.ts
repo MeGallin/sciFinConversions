ede93b8f0e567f301ee8c20d46645215fe215fdb
Guy <me@garyallin.uk>
2018-10-29 09:25:56 +0000

    Working: sticky footer and added cards to components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  public innerWidth: number;
 
-  constructor() { }
+  @HostListener("window:resize", ["$event"])
+  onresize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+  constructor() {}
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
   }
-
 }
