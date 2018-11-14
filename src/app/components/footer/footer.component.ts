b64ba70a159561a9b9af15ea7c3d86b92e05538b
Guy <me@garyallin.uk>
2018-11-14 11:13:11 +0000

    Added calendar component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { DateTimeService } from 'src/app/services/date-time.service';
+import { Component, OnInit } from "@angular/core";
+import { DateTimeService } from "src/app/services/date-time.service";
+import { HttpGetService } from "src/app/services/http-get.service";
+import { interval } from "rxjs";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
   showTime;
+  showBilliton;
+  height: number = 260;
 
-  constructor(private dateTime$ : DateTimeService) { }
+  constructor(
+    private dateTime$: DateTimeService,
+    private billitonService$: HttpGetService
+  ) {}
 
   ngOnInit() {
-  this.showTime =  this.dateTime$.dateTime();
+ this.billitonService$.getBilliton().subscribe(res => {
+   this.showBilliton = res;
+ })
+    this.showTime = this.dateTime$.dateTime();
   }
-
 }
